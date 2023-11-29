8293ec313ba5687c254feff350cea80bc886ccc3
IqraAbbasi <139046452+iqra-abbasi@users.noreply.github.com>
2023-11-29 16:41:10 +0500

    login page for student in web application using database

---
+++ studentdb/studentdb/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace studentdb
+{
+    partial class StudentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtreg = new System.Windows.Forms.TextBox();
+            this.txtcourse = new System.Windows.Forms.TextBox();
+            this.txtmarks = new System.Windows.Forms.TextBox();
+            this.txtsemister = new System.Windows.Forms.TextBox();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider3 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Snap ITC", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(228, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "STUDENT FORM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(131, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(130, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(131, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "RegNo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(100, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "CourseName";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(105, 239);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 22);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "MidMarks";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(87, 285);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 22);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "SemesterNo";
+            // 
+            // txtid
+            // 
+            this.txtid.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtid.Location = new System.Drawing.Point(220, 69);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(274, 26);
+            this.txtid.TabIndex = 7;
+            this.txtid.TextChanged += new System.EventHandler(this.txtid_TextChanged);
+            // 
+            // txtname
+            // 
+            this.txtname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtname.Location = new System.Drawing.Point(220, 111);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(274, 26);
+            this.txtname.TabIndex = 8;
+            // 
+            // txtreg
+            // 
+            this.txtreg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtreg.Location = new System.Drawing.Point(220, 155);
+            this.txtreg.Name = "txtreg";
+            this.txtreg.Size = new System.Drawing.Size(274, 26);
+            this.txtreg.TabIndex = 9;
+            // 
+            // txtcourse
+            // 
+            this.txtcourse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcourse.Location = new System.Drawing.Point(220, 195);
+            this.txtcourse.Name = "txtcourse";
+            this.txtcourse.Size = new System.Drawing.Size(274, 26);
+            this.txtcourse.TabIndex = 10;
+            // 
+            // txtmarks
+            // 
+            this.txtmarks.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmarks.Location = new System.Drawing.Point(220, 239);
+            this.txtmarks.Name = "txtmarks";
+            this.txtmarks.Size = new System.Drawing.Size(274, 26);
+            this.txtmarks.TabIndex = 11;
+            // 
+            // txtsemister
+            // 
+            this.txtsemister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsemister.Location = new System.Drawing.Point(220, 281);
+            this.txtsemister.Name = "txtsemister";
+            this.txtsemister.Size = new System.Drawing.Size(274, 26);
+            this.txtsemister.TabIndex = 12;
+            // 
+            // btnadd
+            // 
+            this.btnadd.BackColor = System.Drawing.Color.Crimson;
+            this.btnadd.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnadd.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnadd.Location = new System.Drawing.Point(266, 322);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(153, 47);
+            this.btnadd.TabIndex = 13;
+            this.btnadd.Text = "ADD";
+            this.btnadd.UseVisualStyleBackColor = false;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // errorProvider3
+            // 
+            this.errorProvider3.ContainerControl = this;
+            // 
+            // StudentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(735, 450);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.txtsemister);
+            this.Controls.Add(this.txtmarks);
+            this.Controls.Add(this.txtcourse);
+            this.Controls.Add(this.txtreg);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "StudentForm";
+            this.Text = "StudentForm";
+            this.Load += new System.EventHandler(this.StudentForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtreg;
+        private System.Windows.Forms.TextBox txtcourse;
+        private System.Windows.Forms.TextBox txtmarks;
+        private System.Windows.Forms.TextBox txtsemister;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.ErrorProvider errorProvider3;
+    }
+}
+
---
+++ studentdb/studentdb/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace studentdb
+{
+    public partial class StudentForm : Form
+    {
+        public StudentForm()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con=new SqlConnection(@"Data Source=DESKTOP-0J02DHK\SQLEXPRESS01;Initial Catalog=studentdb;Integrated Security=True");
+        SqlCommand cmd;
+
+      
+
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+
+
+
+            /* if(string.IsNullOrEmpty(txtname.Text.Trim()))
+ {
+     txtname.Focus();
+     this.BackColor = Color.Red;
+     errorProvider1.SetError(txtname, "student name required!");
+     return;
+ }
+ else
+ {
+     errorProvider1.Clear();
+ }
+
+ if(txtreg.Text.Length<12)
+ {
+     txtreg.Focus();
+     errorProvider2.SetError(txtreg, "Registration Number Must be Greater Then 12");
+     return;
+ }
+ else
+ {
+     errorProvider2.Clear();
+ }
+ if (!int.TryParse(txtmarks.Text, out int marks) || marks < 0)
+ {
+     txtmarks.Focus();
+     this.BackColor = Color.Green;
+     errorProvider3.SetError(txtmarks, "Marks Should Not be Negative!");
+     return;
+ }
+ else
+ {
+     errorProvider3.Clear();
+ }*/
+
+
+
+
+            if (string.IsNullOrEmpty(txtname.Text.Trim()))
+            {
+                this.BackColor = Color.LightBlue;
+                MessageBox.Show("Student name cannot be empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if(txtreg.Text.Length!=12)
+            {
+                MessageBox.Show("Registration number must be exactly 12 characters","Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!int.TryParse(txtmarks.Text, out int marks) || marks < 0)
+            {
+                this.BackColor=Color.Green;
+                MessageBox.Show("Marks should be a non-negative integer.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+
+
+
+            DialogResult result = MessageBox.Show("Do you really want to save the record?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+
+            if (result == DialogResult.Yes)
+            {
+                con.Open();
+
+                cmd = new SqlCommand("INSERT INTO student(ID,Name,RegNo,CourseName,MidMarks,Semister) VALUES (@ID,@Name,@RegNo,@CourseName,@MidMarks,@Semister)", con);
+                cmd.Parameters.Add("@ID", Convert.ToInt64(txtid.Text));
+                cmd.Parameters.Add("@Name", txtname.Text);
+                cmd.Parameters.Add("@RegNo", txtreg.Text);
+                cmd.Parameters.Add("@CourseName", txtcourse.Text);
+                cmd.Parameters.Add("@MidMarks", txtmarks.Text);
+                cmd.Parameters.Add("@Semister", Convert.ToInt32(txtsemister.Text));
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record successfully saved!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+
+            else
+            {
+                MessageBox.Show("Record not saved.", "Cancelled", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void txtid_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StudentForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
