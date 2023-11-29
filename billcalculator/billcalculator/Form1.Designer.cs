27b38c480d60a9c1f773882bdf0060cb815d58b4
IqraAbbasi <139046452+iqra-abbasi@users.noreply.github.com>
2023-11-29 16:35:36 +0500

    bill calculate

---
+++ billcalculator/billcalculator/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace billcalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.btncalculate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtunits = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(220, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(441, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ELECTRICITY BILL CAULATOR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(221, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Your Name";
+            // 
+            // txtname
+            // 
+            this.txtname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtname.Location = new System.Drawing.Point(375, 103);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(214, 26);
+            this.txtname.TabIndex = 2;
+            // 
+            // btncalculate
+            // 
+            this.btncalculate.BackColor = System.Drawing.Color.Maroon;
+            this.btncalculate.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalculate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btncalculate.Location = new System.Drawing.Point(395, 208);
+            this.btncalculate.Name = "btncalculate";
+            this.btncalculate.Size = new System.Drawing.Size(149, 42);
+            this.btncalculate.TabIndex = 3;
+            this.btncalculate.Text = "Calculate";
+            this.btncalculate.UseVisualStyleBackColor = false;
+            this.btncalculate.Click += new System.EventHandler(this.btncalculate_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(221, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Enter Total units";
+            // 
+            // txtunits
+            // 
+            this.txtunits.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtunits.Location = new System.Drawing.Point(375, 158);
+            this.txtunits.Name = "txtunits";
+            this.txtunits.Size = new System.Drawing.Size(214, 26);
+            this.txtunits.TabIndex = 5;
+            this.txtunits.TextChanged += new System.EventHandler(this.txtunits_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtunits);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btncalculate);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ElectricityBillCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Button btncalculate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtunits;
+    }
+}
+
---
+++ billcalculator/billcalculator/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace billcalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        double inputunits;
+        double surcharge = 0.05;
+        double netbill;
+        int costperunit = 5;
+        double billperunit;
+        int tvcharges = 35;
+        double totalbill;
+        double mytvcharges;
+
+
+
+
+        private void btncalculate_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtname.Text.Trim()))
+            {
+                MessageBox.Show("name required", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtunits.Text.Trim()))
+            {
+                MessageBox.Show("units cannot be null", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!int.TryParse(txtunits.Text, out int totalUnits)|| totalUnits <0)
+            {
+                MessageBox.Show("Please enter valid units", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+            try
+            {
+
+                if (inputunits > 500)
+                {
+
+                    netbill = inputunits * surcharge;//adding 5%
+                    billperunit = netbill * costperunit;//adding per unit price
+                    mytvcharges = billperunit + tvcharges;
+                    totalbill = mytvcharges;
+
+                    MessageBox.Show($"Name: {txtname.Text}  \n  Total Units: {inputunits} \n  Cost per Unit: Rs. {costperunit}\nTotal Bill: Rs. {totalbill}",  "Bill Details");
+
+                  
+
+
+
+                }
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+              
+           
+
+
+
+
+        }
+
+        private void txtunits_TextChanged(object sender, EventArgs e)
+        {
+            inputunits = Convert.ToDouble(txtunits.Text);
+        }
+    }
+}
